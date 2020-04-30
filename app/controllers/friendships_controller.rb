f3d712b2568939350dbfa71af9f1a672dde71e71
Luis Saavedra <nriqu322@gmail.com>
2020-04-30 15:41:30 -0600

    add accept friends to controllers and view

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -7,7 +7,13 @@ class FriendshipsController < ApplicationController
 
     def edit
         fs = Friendship.where(user_id: params[:id], friend_id: current_user.id)
-        Friendship.destroy(fs.ids)
+        if params[:choice] == '0'
+            Friendship.destroy(fs.ids)
+        elsif params[:choice] == '1'
+            friendship = Friendship.find_by_id(fs.ids)
+            friendship.confirmed = true
+            friendship.save
+        end
         redirect_to users_path
     end
 end
