f8c8702b6fd26ba4bf46dccb4c0e4180032e5b13
Luis Saavedra <nriqu322@gmail.com>
2020-05-04 11:38:43 -0600

    delete comments in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -13,27 +11,20 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
-  # Find Friends
   def friends
-    # find friends in friend column (as requester)
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-    # find friends in user column (as requested)
     friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
-    # clean array from nil values
     friends_array.compact
   end
 
-  # Users who have yet to confirm friend requests (the one that sends the invite)
   def pending_friends
     friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
   end
 
-  # Users who have requested to be friends (the one that receives the invite checks who send the invite)
   def friend_requests
     inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
   end
 
-  # Fiend a specific friend
   def friend?(user)
     friends.include?(user)
   end
