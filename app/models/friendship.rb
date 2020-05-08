7bf374f6bc1de83269ea2b864961d660954c2f2d
Luis Saavedra <nriqu322@gmail.com>
2020-05-07 18:25:09 -0600

    add unit tests for duplicate and self friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,14 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  validate :friendship_valid?
+
+  def friendship_valid?
+    if (Friendship.where(user_id: user_id, friend_id: friend_id, confirmed: true).exists? ||
+    Friendship.where(user_id: friend_id, friend_id: user_id, confirmed: true).exists? ||
+    user_id == friend_id) 
+    errors.add(:user_id, "cannot have duplicate id's")
+    end
+  end 
 end
